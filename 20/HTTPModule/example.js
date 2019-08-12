b89ec09644c44d2b6256fa6ac51c4f46fd1d0fd8
= <josephalanchandlerjr@gmail.com>
2019-08-12 11:39:28 -0400

    add http request example

--- 20/HTTPModule/example.js
+++ 20/HTTPModule/example.js
@@ -1,12 +1,31 @@
-const {createServer} = require("http");
+const {createServer, request} = require("http");
 
-let server = createServer((request, response)=> {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(`
-    <h1>Hello!</h1>
-    <p>You asked for <code>${request.url}</code></p>`);
-    response.end();
-});
+function runServer(){
+  let server = createServer((request, response)=> {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(`
+      <h1>Hello!</h1>
+      <p>You asked for <code>${request.url}</code></p>`);
+      response.end();
+  });
 
-server.listen(8000);
-console.log("Listening! (port 8000)");
+  server.listen(8000);
+  console.log("Listening! (port 8000)");
+}
+
+function makeRequest(){
+  let requestStream = request({
+  hostname: "eloquentjavascript.net",
+  path: "/20_node.html",
+  method: "GET",
+  headers: {Accept: "text/html"}}, response => {
+    console.log("Server responded with status code", response.statusCode);
+  });
+  requestStream.end();
+}
+let arg = process.argv[2];
+
+switch(arg){
+  case "makeRequest": makeRequest(); break;
+  case "runServer": runServer(); break;
+}
